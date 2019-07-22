fd75537eac0e92a52fc4e057c9dc79769ca06221
Dan Slimmon <dslimmon@hashicorp.com>
2019-07-22 13:49:39 +0000

    fixed typo in resume and made deploy.sh more general

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 cd $(dirname $0)
-source ~/venvs/aws/bin/activate
 aws s3 sync content/ s3://danslimmon-dotcom/
 aws cloudfront create-invalidation --distribution-id E1I0FBV3Z4XP2X --paths '/*'
